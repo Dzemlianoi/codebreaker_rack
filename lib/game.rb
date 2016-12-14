c997c89ed642d2fbe94c1580d0d197c3f8bb6c49
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-12-14 10:14:56 +0200

    changes pack #3

--- lib/game.rb
+++ lib/game.rb
@@ -1,76 +1,75 @@
 module Codebreaker
   class Game
     attr_accessor :current_code, :secret_code, :difficulties, :difficulty,
-                  :attempts_left, :hints_left, :hint_code_digits, :name, :attempts_array
+                  :attempts_left, :hints_left, :hint_code_digits, :name,
+                  :attempts_array, :hints_array
 
     def initialize
-      self.difficulties = Loader.load('difficulties')
+      @difficulties = Loader.load('difficulties')
     end
 
-    def asign_start_game_options (name, difficulty)
-      self.name = name
-      self.difficulty= difficulty.to_sym
-      self.hints_left = difficulty_info[:hints]
-      self.attempts_left = difficulty_info[:attempts]
-      self.secret_code = generate_secret_code
-      self.hint_code_digits = secret_code.clone
+    def init_game_options (name, difficulty)
+      @name = name
+      @difficulty= difficulty.to_sym
+      @hints_left = difficulty_info[:hints]
+      @attempts_left = difficulty_info[:attempts]
+      @secret_code = generate_secret_code
+      @hint_code_digits = secret_code.clone
+      @attempts_array = []
+      @hints_array = []
+      to_h
+    end
+
+    def init_started_game_options(options)
+      options.each do |k,v|
+        instance_variable_set("@#{k}", v)
+      end
       to_h
     end
 
     def get_hint
       return unless hints_left?
-      self.hints_left -= 1
-      get_hint_digit
+      @hints_left -= 1
+      hint = get_hint_digit
+      @hints_array.push(hint)
     end
 
     def hints_left?
-      self.hints_left > 0
+      @hints_left > 0
     end
 
     def code_operations(current_code)
-      self.current_code = current_code
-      self.attempts_left -= 1
-      marking_result
+      @current_code = current_code
+      @attempts_left -= 1
+      @attempts_array.push(@current_code)
     end
 
     def win?
-      current_code == secret_code
+      @current_code == @secret_code
     end
 
     def to_h
       {
-          name: name,
-          difficulty: difficulty,
-          attempts: difficulty_info[:attempts],
-          attempts_left: attempts_left,
-          hints: difficulty_info[:hints],
-          hints_left: hints_left,
-          secret_code: secret_code
+          name: @name,
+          difficulty: @difficulty,
+          attempts_left: @attempts_left,
+          hints_left: @hints_left,
+          secret_code: @secret_code,
+          hints_array: @hints_array,
+          attempts_array: @attempts_array,
+          hint_code_digits: @hint_code_digits
+
       }
     end
 
     def difficulty_info
-      difficulties[self.difficulty]
+      difficulties[@difficulty]
     end
 
-    private
-
-    def generate_secret_code
-      Array.new(4) { rand(0..6)}.join
-    end
-
-    def get_hint_digit
-      hint_code_digits.slice!(rand(hint_code_digits.size))
-    end
-
-    def attempts_left?
-      attempts_left > 0
-    end
-
-    def marking_result
+    def marking_result(code)
       answer = ''
-      secret_copy = secret_code.split('')
-      current_copy = current_code.split('')
+      secret_copy = @secret_code.split('')
+      current_copy = code.split('')
       secret_copy.each_with_index do |val, key|
         next unless val == current_copy[key]
         current_copy[key], secret_copy[key] = nil
@@ -84,8 +83,21 @@ module Codebreaker
         answer << '-'
         secret_copy[secret_copy.find_index(digit)] = nil
       end
-
       answer
+    end
+
+    private
+
+    def generate_secret_code
+      Array.new(4) { rand(0..6)}.join
+    end
+
+    def get_hint_digit
+      @hint_code_digits.slice!(rand(@hint_code_digits.size))
+    end
+
+    def attempts_left?
+      @attempts_left > 0
     end
   end
 end
--- lib/racker.rb
+++ lib/racker.rb
@@ -14,30 +14,73 @@ class Racker
   end
 
   def response
+    @game = Codebreaker::Game.new
     case @request.path
       when '/'
-        intro if first_play?
+        intro
       when '/options'
-        @game = Codebreaker::Game.new
-        @subtemplate = 'templates/options.html.erb'
-        Rack::Response.new(render('intro.html.erb'))
+        ask_options
+      when '/confirm_options'
+        init_game_options
       when '/game'
-        return Rack::response.new{|redirect| redirect.redirect('/options')} if @request.params['name'].nil?
-        @subtemplate = 'templates/game.html.erb'
-        Rack::Response.new(render('intro.html.erb'))
+        setting_game_options
+        render_game_page
+      when '/hint'
+        hint
+      when '/guess'
+        guess
       else
         Rack::Response.new('Not Found', 404)
     end
   end
 
-  def game_options
+  def first_play?
+    true
+  end
+
+  def ask_options
+    @subtemplate = 'templates/options.html.erb'
+    Rack::Response.new(render('intro.html.erb'))
+  end
+
+  def hint
+    @game.init_started_game_options(get_cookie)
+    @game.get_hint
+    set_cookie(@game.to_h)
+  end
+
+  def init_game_options
     name = @request.params['name']
     difficulty = @request.params['difficulty']
-    options = @game.asign_game_options(name, difficulty)
-    Rack::Response.new do |response|
-      response.set_cookie('game', options.to_json)
-      response.redirect('/game')
-    end
+    options = @game.init_game_options(name, difficulty)
+    set_cookie(options)
+  end
+
+  def win
+    Rack::Response.new('Win, bro', 200)
+  end
+
+  def loose
+    Rack::Response.new('Loose, bro', 200)
+  end
+
+  def render_game_page
+    @subtemplate = 'templates/game.html.erb'
+    Rack::Response.new(render('intro.html.erb'))
+  end
+
+  def setting_game_options
+    puts 'it'
+    @game.init_started_game_options(get_cookie)
+  end
+
+  def guess
+    @game.init_started_game_options(get_cookie)
+    @game.current_code = @request.params['code']
+    return win if @game.win?
+    return loose if @game.attempts_left < 0
+    @game.code_operations(@request.params['code'])
+    set_cookie(@game.to_h)
   end
 
   def intro
@@ -45,17 +88,19 @@ class Racker
     Rack::Response.new(render('intro.html.erb'))
   end
 
-  def first_play?
-    true
-  end
-
-  def asign_options
-    @options = JSON.parse(@request['params'])
-    @game.asign_start_game_options
-  end
-
   def render(template)
     path = File.expand_path("../views/#{template}", __FILE__)
     ERB.new(File.read(path)).result(binding)
+  end
+
+  def set_cookie(options)
+    Rack::Response.new do |response|
+      response.set_cookie('game', options.to_json)
+      response.redirect('/game')
+    end
+  end
+
+  def get_cookie()
+    JSON.parse(@request.cookies['game'])
   end
 end
