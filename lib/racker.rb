47925ccfec153c056847a54ef5e532ae110d3f96
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-12-14 15:03:43 +0200

    changes pack #3

--- lib/racker.rb
+++ lib/racker.rb
@@ -17,76 +17,83 @@ class Racker
     @game = Codebreaker::Game.new
     case @request.path
       when '/'
-        intro
+        render_with_template('main')
       when '/options'
-        ask_options
+        render_with_template('options')
       when '/confirm_options'
         init_game_options
       when '/game'
         setting_game_options
-        render_game_page
+        render_with_template('game')
       when '/hint'
         hint
       when '/guess'
         guess
+      when '/win'
+        return redirect unless game_started?
+        render_with_template('win')
+      when '/loose'
+        return redirect unless game_started?
+        render_with_template('loose')
+      when '/restart'
+        return redirect unless game_started?
+        restart
+      when '/save'
+        return redirect unless game_started?
+        save_result
       else
         Rack::Response.new('Not Found', 404)
     end
   end
 
-  def first_play?
-    true
-  end
-
-  def ask_options
-    @subtemplate = 'templates/options.html.erb'
-    Rack::Response.new(render('intro.html.erb'))
-  end
-
-  def hint
-    @game.init_started_game_options(get_cookie)
-    @game.get_hint
-    set_cookie(@game.to_h)
+  def render_with_template(subtemplate = nil, template = 'intro')
+    @subtemplate = "templates/#{subtemplate}.html.erb" unless subtemplate.nil?
+    Rack::Response.new(render("#{template}.html.erb"))
   end
 
   def init_game_options
+    return redirect unless @request.params.key?('name')
     name = @request.params['name']
     difficulty = @request.params['difficulty']
     options = @game.init_game_options(name, difficulty)
     set_cookie(options)
   end
 
-  def win
-    Rack::Response.new('Win, bro', 200)
-  end
-
-  def loose
-    Rack::Response.new('Loose, bro', 200)
-  end
-
-  def render_game_page
-    @subtemplate = 'templates/game.html.erb'
-    Rack::Response.new(render('intro.html.erb'))
-  end
-
   def setting_game_options
-    puts 'it'
     @game.init_started_game_options(get_cookie)
+  end
+
+  def hint
+    return redirect unless game_started?
+    @game.init_started_game_options(get_cookie)
+    @game.get_hint
+    set_cookie(@game.to_h)
   end
 
   def guess
-    @game.init_started_game_options(get_cookie)
+    return redirect unless game_started?
+    setting_game_options
     @game.current_code = @request.params['code']
-    return win if @game.win?
-    return loose if @game.attempts_left < 0
+    return redirect('win') if @game.win?
+    return redirect('loose') if @game.loose?
     @game.code_operations(@request.params['code'])
     set_cookie(@game.to_h)
   end
 
-  def intro
-    @subtemplate = 'templates/non_reg_main.html.erb'
-    Rack::Response.new(render('intro.html.erb'))
+  def restart
+    Rack::Response.new do |response|
+      response.delete_cookie('game')
+      response.redirect('/')
+    end
   end
+
+  def save_result
+    return redirect unless game_started?
+    Codebreaker::Loader.save('stats', get_cookie)
+    restart
+  end
+
+  private
 
   def render(template)
     path = File.expand_path("../views/#{template}", __FILE__)
@@ -102,5 +109,15 @@ class Racker
 
   def get_cookie()
     JSON.parse(@request.cookies['game'])
+  end
+
+  def redirect(where = '')
+    Rack::Response.new do |response|
+      response.redirect("/#{where}")
+    end
+  end
+
+  def game_started?
+    @request.cookies.key?('game')
   end
 end
