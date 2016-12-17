508b9837d2a4d9762f0bf722042036bbade8ec9b
Denis Zemlianoi <dzemlianoi@gmail.com>
2016-12-17 14:43:35 +0200

    version updated

--- lib/codebreaker_web.rb
+++ lib/codebreaker_web.rb
@@ -15,101 +15,77 @@ module CodebreakerWeb
     end
 
     def response
-      @game = ::Codebreaker::Game.new
       case @request.path
         when '/'
           render_with_template('main')
         when '/options'
+          @difficulties = ::Codebreaker::Loader.load('difficulties')
           render_with_template('options')
-        when '/confirm_options'
-          init_game_options
+        when '/confirm_settings'
+          confirm_settings
         when '/game'
-          setting_game_options
           render_with_template('game')
         when '/hint'
           hint
         when '/guess'
           guess
         when '/win'
-          return redirect unless game_started?
           render_with_template('win')
         when '/loose'
-          return redirect unless game_started?
           render_with_template('loose')
-        when '/restart'
-          return redirect unless game_started?
-          restart
         when '/save'
-          return redirect unless game_started?
-          save_result
+          protected_game_actions('save_result')
+        when '/restart'
+          restart
         else
           Rack::Response.new('Not Found', 404)
       end
     end
 
-    def render_with_template(subtemplate = nil, template = 'intro')
-      @subtemplate = "templates/#{subtemplate}.html.erb" unless subtemplate.nil?
-      Rack::Response.new(render("#{template}.html.erb"))
-    end
-
-    def init_game_options
+    def confirm_settings
       return redirect unless @request.params.key?('name')
       name = @request.params['name']
       difficulty = @request.params['difficulty']
-      options = @game.init_game_options(name, difficulty)
-      set_cookie(options)
-    end
-
-    def setting_game_options
-      @game.init_started_game_options(get_cookie)
+      @request.session[:game] = ::Codebreaker::Game.new(name,difficulty)
+      redirect('game')
     end
 
     def hint
-      return redirect unless game_started?
-      @game.init_started_game_options(get_cookie)
-      @game.get_hint
-      set_cookie(@game.to_h)
+      game.get_hint
+      redirect('game')
     end
 
     def guess
-      return redirect unless game_started?
-      setting_game_options
-      @game.current_code = @request.params['code']
-      return redirect('win') if @game.win?
-      return redirect('loose') if @game.loose?
-      @game.code_operations(@request.params['code'])
-      set_cookie(@game.to_h)
-    end
-
-    def restart
-      Rack::Response.new do |response|
-        response.delete_cookie('game')
-        response.redirect('/')
-      end
+      return redirect('win') if game.win?(@request.params['code'])
+      game.code_operations @request.params['code']
+      return redirect('loose')  if game.loose?
+      redirect('game')
     end
 
     def save_result
-      return redirect unless game_started?
-      ::Codebreaker::Loader.save('stats', get_cookie)
+      ::Codebreaker::Loader.save('stats', game.to_h)
       restart
     end
 
     private
+
+    def game
+      @request.session[:game]
+    end
 
     def render(template)
       path = File.expand_path("../views/#{template}", __FILE__)
       ERB.new(File.read(path)).result(binding)
     end
 
-    def set_cookie(options)
-      Rack::Response.new do |response|
-        response.set_cookie('game', options.to_json)
-        response.redirect('/game')
-      end
+    def render_with_template (subtemplate = nil, template = 'intro')
+      @subtemplate = "templates/#{subtemplate}.html.erb" unless subtemplate.nil?
+      Rack::Response.new(render("#{template}.html.erb"))
     end
 
-    def get_cookie()
-      JSON.parse(@request.cookies['game'])
+    def restart
+      @request.session.clear
+      redirect
     end
 
     def redirect(where = '')
@@ -119,7 +95,7 @@ module CodebreakerWeb
     end
 
     def game_started?
-      @request.cookies.key?('game')
+      @request.session.key?(:game)
     end
   end
 end
